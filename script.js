051bdffcb73906a7c2e5c02de50c983d002fed53
Cipher <shafiudin55@gmail.com>
2026-05-25 15:38:28 +0700

    Add home and TikTok menu functionality

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 console.log("CIPHER READY");
+
+const homeMenu = document.getElementById("home-menu");
+const tiktokMenu = document.getElementById("tiktok-menu");
+
+const homePage = document.getElementById("home-page");
+const tiktokPage = document.getElementById("tiktok-page");
+
+/* HOME */
+
+homeMenu.onclick = () => {
+
+    homePage.classList.remove("hidden");
+    tiktokPage.classList.add("hidden");
+
+}
+
+/* TIKTOK */
+
+tiktokMenu.onclick = () => {
+
+    tiktokPage.classList.remove("hidden");
+    homePage.classList.add("hidden");
+
+}
