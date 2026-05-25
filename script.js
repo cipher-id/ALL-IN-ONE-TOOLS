f57b2e8ef148e031a2b437981548ed759e1e55a1
Cipher <shafiudin55@gmail.com>
2026-05-25 15:42:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
-console.log("CIPHER READY");
-
 const homeMenu = document.getElementById("home-menu");
 const tiktokMenu = document.getElementById("tiktok-menu");
 
 const homePage = document.getElementById("home-page");
 const tiktokPage = document.getElementById("tiktok-page");
 
-/* HOME */
+const navbarTitle = document.getElementById("navbar-title");
 
-homeMenu.onclick = () => {
+/* HOME MENU */
 
+homeMenu.addEventListener("click", () => {
+
+    /* PAGE */
     homePage.classList.remove("hidden");
     tiktokPage.classList.add("hidden");
 
-}
+    /* NAVBAR */
+    navbarTitle.innerText = "HOME";
 
-/* TIKTOK */
+    /* ACTIVE BUTTON */
+    homeMenu.classList.add("active");
+    tiktokMenu.classList.remove("active");
 
-tiktokMenu.onclick = () => {
+});
 
+/* TIKTOK MENU */
+
+tiktokMenu.addEventListener("click", () => {
+
+    /* PAGE */
     tiktokPage.classList.remove("hidden");
     homePage.classList.add("hidden");
 
-}
+    /* NAVBAR */
+    navbarTitle.innerText = "TIKTOK";
+
+    /* ACTIVE BUTTON */
+    tiktokMenu.classList.add("active");
+    homeMenu.classList.remove("active");
+
+});
