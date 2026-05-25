5bebf2d89139d7a1f66199aa5ab536b2ca22b598
Cipher <shafiudin55@gmail.com>
2026-05-25 16:22:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,93 @@
 const homeMenu = document.getElementById("home-menu");
 const tiktokMenu = document.getElementById("tiktok-menu");
+const updateMenu = document.getElementById("update-menu");
+const settingsMenu = document.getElementById("settings-menu");
 
 const homePage = document.getElementById("home-page");
 const tiktokPage = document.getElementById("tiktok-page");
+const updatePage = document.getElementById("update-page");
+const settingsPage = document.getElementById("settings-page");
 
 const navbarTitle = document.getElementById("navbar-title");
 
-/* HOME MENU */
+/* ALL PAGES */
+
+function hideAllPages(){
+
+    homePage.classList.add("hidden");
+    tiktokPage.classList.add("hidden");
+    updatePage.classList.add("hidden");
+    settingsPage.classList.add("hidden");
+
+}
+
+/* REMOVE ACTIVE */
+
+function removeActive(){
+
+    homeMenu.classList.remove("active");
+    tiktokMenu.classList.remove("active");
+    updateMenu.classList.remove("active");
+    settingsMenu.classList.remove("active");
+
+}
+
+/* HOME */
 
 homeMenu.addEventListener("click", () => {
 
-    /* PAGE */
-    homePage.classList.remove("hidden");
-    tiktokPage.classList.add("hidden");
+    hideAllPages();
+    removeActive();
 
-    /* NAVBAR */
+    homePage.classList.remove("hidden");
+
     navbarTitle.innerText = "HOME";
 
-    /* ACTIVE BUTTON */
     homeMenu.classList.add("active");
-    tiktokMenu.classList.remove("active");
 
 });
 
-/* TIKTOK MENU */
+/* TIKTOK */
 
 tiktokMenu.addEventListener("click", () => {
 
-    /* PAGE */
-    tiktokPage.classList.remove("hidden");
-    homePage.classList.add("hidden");
+    hideAllPages();
+    removeActive();
 
-    /* NAVBAR */
+    tiktokPage.classList.remove("hidden");
+
     navbarTitle.innerText = "TIKTOK";
 
-    /* ACTIVE BUTTON */
     tiktokMenu.classList.add("active");
-    homeMenu.classList.remove("active");
+
+});
+
+/* UPDATE */
+
+updateMenu.addEventListener("click", () => {
+
+    hideAllPages();
+    removeActive();
+
+    updatePage.classList.remove("hidden");
+
+    navbarTitle.innerText = "next update";
+
+    updateMenu.classList.add("active");
+
+});
+
+/* SETTINGS */
+
+settingsMenu.addEventListener("click", () => {
+
+    hideAllPages();
+    removeActive();
+
+    settingsPage.classList.remove("hidden");
+
+    navbarTitle.innerText = "setelan";
+
+    settingsMenu.classList.add("active");
 
 });
